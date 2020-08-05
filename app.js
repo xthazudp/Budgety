5d420f7b2bb9fe04b7c6c94da48d3157eee9ee68
xthazudp <xthazudp@gmail.com>
2020-08-05 21:06:59 +0545

    init fuction creation

--- app.js
+++ app.js
@@ -34,13 +34,25 @@ var UIController = (function(){
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event){
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
+
+
 
     var ctrlAddItem = function(){
 
         // 1. get the field input data
         var input = UICtrl.getInput();
-        console.log(input);
+        
 
         // 2. add the item to the budget controller
         // 3. add the item to the UI
@@ -48,18 +60,15 @@ var controller = (function(budgetCtrl, UICtrl){
         // 5. display the budget
 
         console.log('It works');
-    }
+    };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event){
-
-        if(event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    return{
+        init: function(){
+            console.log('app is started.');
+            setupEventListeners();
         }
-
-
-    });
+    }
 
 })(budgetController, UIController);
 
+controller.init();
