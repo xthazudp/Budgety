ee202e6ae280ce831cefc78c707335b2f0ad636a
xthazudp <xthazudp@gmail.com>
2020-08-07 18:17:44 +0545

    delete items

--- app.js
+++ app.js
@@ -62,6 +62,22 @@ var budgetController = (function(){
             return newItem;
         },
 
+        deleteItem: function(type, id){
+
+            var ids, index; 
+
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index  = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+
+        },
+
         calculateBudget: function(){
 
             // calculate total income and expenses
@@ -150,6 +166,13 @@ var UIController = (function(){
 
             // insert the html into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+        },
+
+        deleteListItem: function(selectorID){
+
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
 
         },
 
@@ -250,14 +273,17 @@ var controller = (function(budgetCtrl, UICtrl){
             // inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. delete the item from the data structure 
+            budgetCtrl.deleteItem(type, ID);
 
             // 2. delete the item from the UI
+            UICtrl.deleteListItem(itemID);
 
             // 3. update and show the new budget
-            
+            updateBudget();
+
         }
 
     };
